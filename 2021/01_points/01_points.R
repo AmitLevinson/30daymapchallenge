52fe654ee93fa7bde15d7acf082c44cfb9dd10cc
AmitLevinson <amitlevinson1@gmail.com>
2021-11-01 20:48:05 +0200

    fix up plot

--- 2021/01_points/01_points.R
+++ 2021/01_points/01_points.R
@@ -23,24 +23,24 @@ telavivggmap <- get_googlemap(center = "tel aviv, israel", zoom = 13,style=c(fea
 
 
 ggmap(telavivggmap)+
-  geom_sf(data = clean_data, aes(fill = TreeAge), inherit.aes = FALSE, size = 2, pch=21, color = "gray35")+
-  scale_fill_brewer(palette = "YlOrBr", na.value="grey", name = "Tree age (Years)")+
+  geom_sf(data = clean_data, aes(color = TreeAge), inherit.aes = FALSE, size = 1.5)+
+  scale_color_brewer(palette = "YlOrBr", na.value="grey", name = "Tree age (Years)")+
   theme_void()+
-  guides(fill = guide_legend(nrow = 1, byrow = TRUE,override.aes = list(size=3)))+
+  guides(color = guide_legend(nrow = 1, byrow = TRUE,override.aes = list(size=3)))+
   labs(title = "Historical and preserved trees across Tel-Aviv",
        subtitle = "",
-       caption = "Data: Israel Ministry of Agriculture | Visualization: @Amit_Levinson")+
+       caption = "Data: Israel's Ministry of Agriculture | Visualization: @Amit_Levinson")+
   theme(
     text = element_text(family = "Segoe UI"),
-    plot.title = element_text(size = 26, family = "Alegreya", hjust = 0.5, face = "bold"),
+    plot.title = element_text(size = 22, family = "Alegreya", hjust = 0.5, face = "bold"),
     plot.subtitle = element_text(size = 8),
     legend.position = "top",
     legend.direction = "horizontal",
     legend.key.size = unit(3,"mm"),
     legend.key.width = unit(5,"mm"),
-    legend.title = element_text(size = 12, color = "gray10"),
-    legend.text = element_text(size = 12, color = "gray25"),
+    legend.title = element_text(size = 11, color = "gray10"),
+    legend.text = element_text(size = 10, color = "gray25"),
     plot.caption = element_text(hjust = 0.5, color = "gray45", size = 10)
   )
 
-ggsave("2021/01_points/trees.png", width = 8, height = 7)
+ggsave("2021/01_points/trees.png",height = 8, width = 6, dpi = 500)
