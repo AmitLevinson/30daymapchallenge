2642b7432bbb020a3d953c361ed6c6ea232d282d
Amit Levinson <amitlevinson1@gmail.com>
2022-11-04 12:06:56 +0200

    update plot with stosp only before train

--- 2022/03_polygons/03_polygons.R
+++ 2022/03_polygons/03_polygons.R
@@ -92,15 +92,27 @@ get_ride_info <- function (rides) {
 # saveRDS(all_rides,file = '03_polygons/all_rides.rds')
 
 all_rides <- readRDS('03_polygons/all_rides.rds')
+# clean is to filter out any stops *after* the train
+all_rides_clean <- all_rides %>% 
+  group_by(gtfs_ride_id) %>% 
+  arrange(gtfs_ride_id, stop_sequence) %>% 
+  mutate(train_stop = ifelse(str_detect(name, 'רכבת רחובות'), stop_sequence, NA)) %>% 
+  tidyr::fill(train_stop, .direction = 'up') %>% 
+  ungroup() %>% 
+  filter(!is.na(train_stop))
+  
 
-get_route_id <- function (ride_id) {
+
+get_route_ids <- function (ride_id) {
   res = GET("https://open-bus-stride-api.hasadna.org.il/gtfs_rides/get",
             query = list(
               id = ride_id)
             )
-  route_from_ride = fromJSON(rawToChar(res$content)) 
+  route_from_ride = fromJSON(rawToChar(res$content)) %>% 
+    do.call(cbind, .) %>% 
+    as.data.frame()
 
-  return(route_from_ride$gtfs_route_id)
+  return(route_from_ride)
   
 }
 
@@ -116,23 +128,21 @@ get_route_info <- function(route_id = NA) {
   return(route_from_ride)
 }
 
-
-collect_route_from_ride <- function(ride_id) {
-  # Sys.sleep(5)
-  route_id <-  get_route_id (ride_id)  
-  route_info <- get_route_info(route_id)
-  return(route_info)
-}
+# Filter this line ref maybe? 33730
+route_ids <-  map_dfr(unique(all_rides$gtfs_ride_id), get_route_ids )
+route_info <- map_dfr(route_ids$gtfs_route_id, get_route_info)
+route_info %>% 
+  left_join(route_ids, by = c('id' = 'gtfs_route_id') ) %>% View()
 
 
 
 # Collect routes and line-refs
-ride_ids <- unique(all_rides$gtfs_ride_id)
+# ride_ids <- unique(all_rides$gtfs_ride_id)
 
-all_routes <- map_dfr(ride_ids,collect_route_from_ride)
+# all_routes <- map_dfr(ride_ids,collect_route_from_ride)
 
 # clean up duplicate stations for plotting.
-clean_rides <- all_rides %>% 
+clean_rides <- all_rides_clean %>% 
   filter(code %in% stops_in_rehovot$code) %>% 
   distinct(lon,lat, .keep_all = T)
 
@@ -175,21 +185,21 @@ train_stop <- bus_stops_sf %>%
 
   ggplot() +
   geom_sf(data = rehovot_sf, fill = NA, lwd  = 0.5, inherit.aes = F)+
-  geom_sf(data = rehovot_voronoi_poloygon_km, aes(fill = area), color = NA)+
-  geom_sf(data = bus_stops_sf, inherit.aes = FALSE, size = 0.05, alpha = 0.8, color = 'gray85')+
+  geom_sf(data = rehovot_voronoi_poloygon_km, aes(fill = area), color = 'gray55', lwd = 0.1)+
+  geom_sf(data = bus_stops_sf, inherit.aes = FALSE, size = 0.25, color = 'gray85')+
   geom_sf_text(data = train_stop, aes(label = label),
-                   inherit.aes = F, family = 'Font Awesome 6 Free Solid', size = 3, show.legend = FALSE, color = 'white')+
+                   inherit.aes = F, family = 'Font Awesome 6 Free Solid', size = 4, show.legend = FALSE, color = 'white')+
   scale_fill_gradientn("Area (km<sup>2</sup>)", 
                        colors=viridis::inferno(10), breaks = c(0.25, 0.5,.75), na.value = 'gray95')+
-  labs(title = "One Bus Ride to Rehovot's Train Station",
-         subtitle = "Nearest bus stop with a direct route to Rehovot's train station. All locations in an area<br>are closer to that bus stop (dot) than to any other stop. Bus stops were extractred from Israel's GTFS<br>in the past two weeks & sampling routes that reach Rehovot's train station from that stop.<br><span style='color:gray35;'>**Gray**</span> areas are filtered for having mostly open fields.",
+  labs(title = "Nearest Bus Stop with a Direct Route to the Train",
+         subtitle = "Nearest bus stop with a direct route to Rehovot's train station. All locations in an area<br>are closer to that bus stop (dot) than to any other stop. Bus stops and routes that reach<br>Rehovot's train station were sampled from GTFS data in the past two weeks.<br><span style='color:gray35;'>**Gray**</span> = NA for having mostly open fields.",
        caption = "Data: Israel's GTFS; made accessible by 'Hasadna' | @Amit_Levinson")+
   theme_minimal()+
   theme(
     text = element_text(family = "Noto Sans", lineheight = 1),
     plot.title = element_text(size = 28, "Lora", face = 'bold'),
     plot.title.position = "plot",
-    plot.subtitle = element_markdown(size = 16, lineheight = 1.1),
+    plot.subtitle = element_markdown(size = 18, lineheight = 1.1),
     plot.caption = element_text(hjust = 0.5, size = 12, color = "gray25"),
     axis.text = element_blank(),
     axis.title = element_blank(),
@@ -201,6 +211,20 @@ train_stop <- bus_stops_sf %>%
     plot.margin = margin(6,2,6,2,"mm")
   )
   
-ggsave('03_polygons/bus_stops.png', width = 13, height = 9)
+ggsave('03_polygons/bus_stops_updated.png', width = 13, height = 9)
 
-
+ggplot(rehovot_voronoi_poloygon_km) +
+  geom_histogram(aes(x= area)) +
+  labs(title = 'Distribution of areas\' size to the nearest bus stop with a direct route\nto train',
+       y= 'Count',
+       x = 'Area size (km^2)')+
+  theme_minimal() +
+  theme(
+    plot.title = element_text(size = 16, "Lora", face = 'bold'),
+    axis.text = element_text(size = 10),
+    axis.title = element_text(size = 13),
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background = element_rect(fill = "white", color = NA)
+  )
+  
+ggsave('03_polygons/area_distribution_updated.png', width = 8, height = 5)
